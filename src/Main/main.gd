753812fb92da7c3a6f52144a52a158a47209d106
4efk <65010619+4efk@users.noreply.github.com>
2024-02-28 08:09:37 +0100

    trying to connect menu button signals

--- src/Main/main.gd
+++ src/Main/main.gd
@@ -1,4 +1,10 @@
 extends CanvasLayer
 
+@onready var file_button = $VBoxContainer/MenuBar/FileButton
+
 func _ready():
-	pass
+	file_button.get_popup().connect("pressed", self, "_file_item_pressed")
+	#file_button.get_popup().connect(_file_item_presses())
+
+func _file_item_pressed(id):
+	print(id)
