faa958eef42fef94bf430d8488672a878ff9946b
4efk <65010619+4efk@users.noreply.github.com>
2024-02-29 08:17:42 +0100

    finally managed to get the font size working

--- src/Main/main.gd
+++ src/Main/main.gd
@@ -3,6 +3,7 @@ extends CanvasLayer
 @onready var file_button = $VBoxContainer/MenuBar/FileButton
 
 func _ready():
+	# connecting menubar buttons
 	file_button.get_popup().id_pressed.connect(_file_item_pressed)
 
 func _file_item_pressed(id):
