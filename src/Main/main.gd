f228bd6acc7c9edb602a42f6b010a879728a2a18
4efk <65010619+4efk@users.noreply.github.com>
2024-02-29 22:13:29 +0100

    added Help menu button and About popup

--- src/Main/main.gd
+++ src/Main/main.gd
@@ -8,18 +8,25 @@ var file_button_functions = [
 	'quit',
 ]
 
+var help_button_functions = [
+	'show_about'
+]
+
 var current_file = null
 
 @onready var file_button = $VBoxContainer/MenuBar/FileButton
+@onready var help_button = $VBoxContainer/MenuBar/HelpButton
 
 @onready var text_edit = $VBoxContainer/TextEdit
 
 @onready var open_file_dialog = $OpenFileDialog
 @onready var save_file_dialog = $SaveFileDialog
+@onready var about_window = $AboutWindow
 
 func _ready():
 	# connecting menubar buttons
 	file_button.get_popup().id_pressed.connect(_file_item_pressed)
+	help_button.get_popup().id_pressed.connect(_help_item_pressed)
 	DisplayServer.window_set_title('Simple Text Editor - Untitled')
 
 func update_window_title():
@@ -27,6 +34,9 @@ func update_window_title():
 		DisplayServer.window_set_title('Simple Text Editor - ' + str(current_file))
 	else:
 		DisplayServer.window_set_title('Simple Text Editor - Untitled')
+
+func show_about():
+	about_window.popup_centered(Vector2(400, 200))
 
 func new_file():
 	current_file = null
@@ -66,3 +76,9 @@ func _on_save_file_dialog_file_selected(path):
 	var f = FileAccess.open(path, FileAccess.WRITE)
 	f.store_string(text_edit.text)
 	f.close()
+	
+func _help_item_pressed(id):
+	call(help_button_functions[id])
+
+func _on_about_window_close_requested():
+	about_window.hide()
