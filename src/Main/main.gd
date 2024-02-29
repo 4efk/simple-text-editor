4e8df86774533adf4d4e9385cd9892bba7095f94
4efk <65010619+4efk@users.noreply.github.com>
2024-02-29 08:51:14 +0100

    asigned functions to the file menu button

--- src/Main/main.gd
+++ src/Main/main.gd
@@ -1,10 +1,29 @@
 extends CanvasLayer
 
+var file_button_functions = [
+	'open_file',
+	'save_file',
+	'save_file_as',
+	'quit',
+]
+
 @onready var file_button = $VBoxContainer/MenuBar/FileButton
+
+func open_file():
+	print(0)
+
+func save_file():
+	print(1)
+
+func save_file_as():
+	print(2)
+
+func quit():
+	get_tree().quit()
 
 func _ready():
 	# connecting menubar buttons
 	file_button.get_popup().id_pressed.connect(_file_item_pressed)
 
 func _file_item_pressed(id):
-	print(id)
+	call(file_button_functions[id])
